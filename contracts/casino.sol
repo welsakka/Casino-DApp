b5c1c01b223c082f5859e4730aebf7f792a5345a
Waleed Elsakka <waleedpahome@live.com>
2018-11-14 13:16:25 -0500

    Cleaned up variables; Added Bet, number generate, and distribute functions

--- contracts/casino.sol
+++ contracts/casino.sol
@@ -1,5 +1,4 @@
 pragma solidity 0.4.24;
-
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
 /// @title Contract to bet Ether for a number and win randomly when the number of bets is met.
@@ -20,11 +19,9 @@ contract Casino is usingOracalize {
     uint256 public numberOfBets;
 
     //MAX allowed bets to be made; to reduce gas consumption when distributing prize
-    uint256 public maxAmountOfBets = 10;
+    uint256 public maxAmountOfBets = 100;
 
-    uint256 public constant limitBets = 100;
-
-    //array of addresses
+    //array of addresses so we may iterate
     address[] public players;
 
     //struct defining player; amount of ether bet and which number the player is betting on
@@ -33,26 +30,61 @@ contract Casino is usingOracalize {
         uint256 numberSelected;
     }
 
-    //the number each player has bet on
+    //info associated with a particular address
     mapping(address => Player) public playerInfo;
-
-    mapping (uint => address[]) public numberBetOn; 
-
-    modifier onEndGame() {
-        if (numberOfBets >= maxAmountOfBets) _;
-    }
 
     constructor (uint256 _minimumBet, uint256 _maxAmountOfBets) public{
         owner = msg.sender;
 
         if (_minimumBet > 0)
             _minimumBet = minimumBet;
-        if (_maxAmountOfBets > 0 && _maxA)
     }
 
-    function makeBet() public payable {
-
+    //Checks if an address is listed in the players array
+    function checkPlayerExists(address player) public view returns(bool){
+        for (uint256 i = 0; i <= players.length; ++i){
+            if (players[i] == player) return true;
+            else
+                return false;
+        }        
     }
+
+    function distributePrizes(uint256 _numberGenerated) public {
+        address[100] memory winners; //temp array to store winner addresses
+        uint256 counter = 0; //counter variable for winner array
+        for (uint256 i = 0; i <= players.length; ++i){
+            if (playerInfo[players[i]].numberSelected == _numberGenerated) {
+                winners[counter] = players[i];
+                ++counter; 
+            }               
+        }
+        for (uint256 j = 0; j <= winners.length; ++j){
+            if(winners[j] != address(0))
+                winners[j].transfer(totalBet / winners.length);
+        }
+    }
+
+    //generate a random number between 1 and 10, inclusive 
+    function generateNumberWinner() public {
+        //takes current block number, modulo it by 10, add 1
+        //ex current block is 128142 % 10 = 2 + 1 = 3
+        uint256 numberGenerated = block.number % 10 + 1; // not secure
+        distributePrizes(numberGenerated);
+    }
+
+    function makeBet(uint256 _numberSelected) public payable {
+        require(!checkPlayerExists(msg.sender));
+        require(_numberSelected >= 1 && _numberSelected <= 10);
+        require(msg.value >= minimumBet);
+        playerInfo[msg.sender].amountBet = msg.value;
+        playerInfo[msg.sender].numberSelected = _numberSelected;
+        numberOfBets++;
+        players.push[msg.sender];
+        totalBet += msg.value;
+        if (numberOfBets >= maxAmountOfBets) generateNumberWinner();
+    }
+
+
 
     function kill() public {
         if (msg.sender == owner) selfdestruct(owner);
