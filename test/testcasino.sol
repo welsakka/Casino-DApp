8097c10202219fa5977bf08d5849bd6747d381a1
Waleed Elsakka <waleedpahome@live.com>
2018-11-24 20:26:15 -0500

    initial commit

---
+++ test/testcasino.sol
@@ -0,0 +1,11 @@
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/casino.sol";
+
+contract Testcasino {
+
+    function testCheckPlayerExists() {
+        
+
+    }
+}
