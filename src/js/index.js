41f3fe22c68646b4dd1531581ce827269ab5ce45
Waleed Elsakka <waleedpahome@live.com>
2018-12-03 11:52:46 -0500

    complete js

--- src/js/index.js
+++ src/js/index.js
@@ -2,42 +2,142 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Web3 from 'web3'
 import './../css/index.css'
+
 class App extends React.Component {
+   
    constructor(props){
       super(props)
       this.state = {
          lastWinner: 0,
-         timer: 0
+         numberOfBets: 0,
+         minimumBet: 0,
+         totalBet: 0,
+         maxAmountOfBets: 0,
       }
+
+      var Web3 = require('web3');
+      var web3 = new Web3();
+      
+      if (typeof web3 !== 'undefined') {
+         App.web3Provider = web3.currentProvider;
+         web3 = new Web3(web3.currentProvider);
+       } else {
+         // If no injected web3 instance is detected, fallback to Ganache.
+         App.web3Provider = new web3.providers.HttpProvider('http://127.0.0.1:7545');
+         web3 = new Web3(App.web3Provider);
+       }
+      const MyContract = web3.eth.contract()
+
+      this.state.ContractInstance = MyContract.at("0x7C2b54c495d053ef20BB082DB74Cbd9d59689C8D")
    }
-voteNumber(number){
-      console.log(number)
+
+   componentDidMount(){
+      this.updateState()
+      this.setupListeners()
+setInterval(this.updateState.bind(this), 10e3)
+   }
+updateState(){
+      this.state.ContractInstance.minimumBet((err, result) => {
+         if(result != null){
+            this.setState({
+               minimumBet: parseFloat(web3.fromWei(result, 'ether'))
+            })
+         }
+      })
+      this.state.ContractInstance.totalBet((err, result) => {
+         if(result != null){
+            this.setState({
+               totalBet: parseFloat(web3.fromWei(result, 'ether'))
+            })
+         }
+      })
+      this.state.ContractInstance.numberOfBets((err, result) => {
+         if(result != null){
+            this.setState({
+               numberOfBets: parseInt(result)
+            })
+         }
+      })
+      this.state.ContractInstance.maxAmountOfBets((err, result) => {
+         if(result != null){
+            this.setState({
+               maxAmountOfBets: parseInt(result)
+            })
+         }
+      })
+   }
+// Listen for events and executes the voteNumber method
+   setupListeners(){
+      let liNodes = this.refs.numbers.querySelectorAll('li')
+      liNodes.forEach(number => {
+         number.addEventListener('click', event => {
+            event.target.className = 'number-selected'
+            this.voteNumber(parseInt(event.target.innerHTML), done => {
+// Remove the other number selected
+               for(let i = 0; i < liNodes.length; i++){
+                  liNodes[i].className = ''
+               }
+            })
+         })
+      })
+   }
+voteNumber(number, cb){
+      let bet = this.refs['ether-bet'].value
+if(!bet) bet = 0.1
+if(parseFloat(bet) < this.state.minimumBet){
+         alert('You must bet more than the minimum')
+         cb()
+      } else {
+         this.state.ContractInstance.bet(number, {
+            gas: 300000,
+            from: web3.eth.accounts[0],
+            value: web3.toWei(bet, 'ether')
+         }, (err, result) => {
+            cb()
+         })
+      }
    }
 render(){
       return (
          <div className="main-container">
             <h1>Bet for your best number and win huge amounts of Ether</h1>
 <div className="block">
-               <h4>Timer:</h4> &nbsp;
-               <span ref="timer"> {this.state.timer}</span>
+               <b>Number of bets:</b> &nbsp;
+               <span>{this.state.numberOfBets}</span>
             </div>
 <div className="block">
-               <h4>Last winner:</h4> &nbsp;
-               <span ref="last-winner">{this.state.lastWinner}</span>
+               <b>Last number winner:</b> &nbsp;
+               <span>{this.state.lastWinner}</span>
+            </div>
+<div className="block">
+               <b>Total ether bet:</b> &nbsp;
+               <span>{this.state.totalBet} ether</span>
+            </div>
+<div className="block">
+               <b>Minimum bet:</b> &nbsp;
+               <span>{this.state.minimumBet} ether</span>
+            </div>
+<div className="block">
+               <b>Max amount of bets:</b> &nbsp;
+               <span>{this.state.maxAmountOfBets} ether</span>
             </div>
 <hr/>
 <h2>Vote for the next number</h2>
-            <ul>
-               <li onClick={() => {this.voteNumber(1)}}>1</li>
-               <li onClick={() => {this.voteNumber(2)}}>2</li>
-               <li onClick={() => {this.voteNumber(3)}}>3</li>
-               <li onClick={() => {this.voteNumber(4)}}>4</li>
-               <li onClick={() => {this.voteNumber(5)}}>5</li>
-               <li onClick={() => {this.voteNumber(6)}}>6</li>
-               <li onClick={() => {this.voteNumber(7)}}>7</li>
-               <li onClick={() => {this.voteNumber(8)}}>8</li>
-               <li onClick={() => {this.voteNumber(9)}}>9</li>
-               <li onClick={() => {this.voteNumber(10)}}>10</li>
+<label>
+               <b>How much Ether do you want to bet? <input className="bet-input" ref="ether-bet" type="number" placeholder={this.state.minimumBet}/></b> ether
+               <br/>
+            </label>
+   <ul ref="numbers">
+               <li>1</li>
+               <li>2</li>
+               <li>3</li>
+               <li>4</li>
+               <li>5</li>
+               <li>6</li>
+               <li>7</li>
+               <li>8</li>
+               <li>9</li>
+               <li>10</li>
             </ul>
          </div>
       )
